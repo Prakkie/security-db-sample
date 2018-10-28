1171240d6b3a987f479faf0c64f21e7830e03e6d
Prakash Srinivasan <prakashsmiles@gmail.com>
2018-10-27 20:36:33 -0600

    initial commit

---
+++ src/main/java/com/example/security/securitydbsample/SecurityDbSampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.security.securitydbsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDbSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityDbSampleApplication.class, args);
+	}
+}
